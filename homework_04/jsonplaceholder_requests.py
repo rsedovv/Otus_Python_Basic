ac3628cb418e72e17ce310419a0c5b2cb45051a6
rsedov <rsedov@ozon.ru>
2025-11-18 14:43:41 +0300

    ДЗ

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -1,21 +1,8 @@
 import aiohttp
-from typing import List, Dict
+from typing import Dict, Any
 
-USERS_DATA_URL = 'https://jsonplaceholder.typicode.com/users'
-POSTS_DATA_URL = 'https://jsonplaceholder.typicode.com/posts'
-
-
-async def fetch_json(session: aiohttp.ClientSession, url: str) -> dict:
-    connector = aiohttp.TCPConnector(ssl=False)  # Отключаем проверку SSL
-    async with aiohttp.ClientSession(connector=connector) as session:
-        async with session.get(url) as response:
-            response.raise_for_status()
-            return await response.json()
-
-
-async def fetch_users_data(session: aiohttp.ClientSession) -> List[Dict]:
-    return await fetch_json(session, USERS_DATA_URL)
-
-
-async def fetch_posts_data(session: aiohttp.ClientSession) -> List[Dict]:
-    return await fetch_json(session, POSTS_DATA_URL)
+async def fetch_json(session: aiohttp.ClientSession, url: str) -> Dict[str, Any]:
+    """Асинхронный HTTP-запрос для получения JSON"""
+    async with session.get(url) as response:
+        response.raise_for_status()
+        return await response.json()
